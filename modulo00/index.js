85d44f1ab598c969758b769142e02aa9c381752c
mayconbalves <mayconmustaine@hotmail.com>
2019-10-15 23:53:12 -0300

    feat(index): create get user and update user

--- modulo00/index.js
+++ modulo00/index.js
@@ -9,10 +9,27 @@ server.get('/users', (req, res) => {
   return res.json(users)
 })
 
+server.get('/users/:index', (res, req) => {
+  const { index } = req.params
+
+  return res.json(users[index])
+})
+
 server.post('/users', (req, res) => {
   const { name } = req.body
 
   users.push(name)
   return res.json(users)
 })
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params
+  const { name } = req.body
+
+  users[index] = name
+
+  return res.json(users)
+})
+
+
 server.listen(3000)
