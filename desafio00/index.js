baa0d2e10e443e931e06b0c3adcb4cc0688dac21
mayconbalves <mayconmustaine@hotmail.com>
2019-10-18 23:10:28 -0300

    feat(index): create post router

--- desafio00/index.js
+++ desafio00/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 
 const server = express()
+server.use(express.json())
+
+const projects = []
 
 server.get('/projects', (req, res) => {
   return res.send('Hello world')
+})
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body
+
+  projects.push({ id, title })
+  return res.json(projects)
 })
 
 server.listen(4000)
