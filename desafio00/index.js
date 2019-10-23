82d6ca8ab25639a31a7ac8c1b2830c1ddb6922dc
mayconbalves <mayconmustaine@hotmail.com>
2019-10-22 22:40:06 -0300

    feat(index): create new post method

--- desafio00/index.js
+++ desafio00/index.js
@@ -46,4 +46,15 @@ server.delete('/projects/:id', checkProjectExists, (req, res) => {
   return res.send()
 })
 
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+
+  const project = projects.find(p => p.id == id)
+
+  project.tasks.push(title)
+
+  return res.json(project)
+})
+
 server.listen(4000)
