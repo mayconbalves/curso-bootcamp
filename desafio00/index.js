08ca113bbdcacf4afc8b388b5c31e88b73b8ad94
mayconbalves <mayconmustaine@hotmail.com>
2019-10-22 22:42:03 -0300

    feat(index): create log middleware

--- desafio00/index.js
+++ desafio00/index.js
@@ -6,15 +6,24 @@ server.use(express.json())
 const projects = []
 
 function checkProjectExists(req, res, next) {
-  const { id } = req.params;
-  const project = projects.find(p => p.id == id);
+  const { id } = req.params
+  const project = projects.find(p => p.id == id)
 
   if (!project) {
-    return res.status(400).json({ error: 'Project not found' });
+    return res.status(400).json({ error: 'Project not found' })
   }
 
-  return next();
+  return next()
 }
+
+function logRequests(req, res, next) {
+
+  console.count("Número de requisições")
+
+  return next()
+}
+
+server.use(logRequests)
 
 server.get('/projects', (req, res) => {
   return res.json(projects)
